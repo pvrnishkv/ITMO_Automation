e07265931c1eddc3c5bd2e218787bec1cab909f6
Egor_Kurguzkin <egor-kurg@yandex.ru>
2025-03-06 18:33:57 +0300

    First Commit

---
+++ python_trening/task_8_if_elif_else.py
@@ -0,0 +1,41 @@
+num_float = 3.4
+
+#num_float = 0 или num_float = -4.5
+
+# if num_float > 0:
+#     print('Положительное число')
+# elif num_float == 0:
+#     print('Ноль')
+# else:
+#     print('Отрицательное число')
+
+# permit_print = True
+# num = 9
+#
+# if num > 0 and permit_print:
+#     print('num - положительное число')
+# elif not permit_print:
+#     print('Печать запрещена')
+
+#Задача
+
+def student_rang(year_of_study):
+    if year_of_study == 1 or year_of_study == 2 or year_of_study == 3 or year_of_study == 4:
+        print('Бакалавр')
+    elif year_of_study in range (5,7):
+        print('Магистр')
+    elif 7<= year_of_study <= 9:
+        print('Аспирант')
+    else:
+        print('Введите корректный год обучения')
+
+student_rang(10)
+
+#Задача
+
+num = 5
+
+if num > 100 or num < -100:
+    print('-')
+else:
+    print('+')
