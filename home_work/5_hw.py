ce0b9d81d5fd1785f2c4fa8a693323dd4451e3b7
Egor_Kurguzkin <egor-kurg@yandex.ru>
2025-03-18 15:41:13 +0300

    Second Commit

---
+++ home_work/5_hw.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+driver = webdriver.Chrome()
+driver.get("https://www.saucedemo.com/")
+
+username = driver.find_element(By.CSS_SELECTOR, '#user-name')
+password = driver.find_element(By.CSS_SELECTOR, '#password')
+submit = driver.find_element(By.CSS_SELECTOR, '#login-button')
+if username is None or password is None or submit is None:
+    print('Элементы не найдены')
+else:
+    print('Элементы найдены')
